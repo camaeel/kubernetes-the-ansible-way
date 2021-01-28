c7490549d0c991242e70a1559a70b3292b160560
Kamil <12999736+camaeel@users.noreply.github.com>
2021-01-28 23:25:31 +0100

    routing, internetgw, nacl

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -23,3 +23,110 @@ resource "aws_subnet" "public" {
     },
   )
 }
+
+resource "aws_internet_gateway" "internet-gw" {
+  vpc_id = aws_vpc.main.id 
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "internet-gw"
+    },
+  )
+} 
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {     
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gw.id
+  }   
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.internet-gw.id
+  }   
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "public"
+    },
+  )
+}
+
+resource "aws_main_route_table_association" "public" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.public.id 
+}
+
+resource "aws_default_network_acl" "default-nacl" {
+  default_network_acl_id = aws_vpc.main.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.main.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 32768
+    to_port    = 61000
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 500
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 600
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 6443
+    to_port    = 6443
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+    tags = merge(
+    var.additional_tags,
+    {
+      Name = "default-nacl"
+    },
+  )
+}
