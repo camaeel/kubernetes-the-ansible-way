1e2c141ffd5b221f594286565644139bd9fb048b
Kamil <12999736+camaeel@users.noreply.github.com>
2021-02-07 20:54:34 +0100

    script to list all hosts from inventory

---
+++ ansible/list_hosts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pushd $(dirname "$0") > /dev/null
+
+ansible-inventory -i inventory.aws_ec2.yml --list | jq '._meta.hostvars[] | {name: .tags.Name, public_ip_address: .public_ip_address, private_ip_address: .private_ip_address}'
+
+popd > /dev/null
