1c1a132ebf7f7a60c636d21459590e57e88f2ac6
Kamil <12999736+camaeel@users.noreply.github.com>
2021-02-07 22:05:15 +0100

    refactoring - move inventory to sub dir generate local .kube/config entry

--- ansible/list_hosts.sh
+++ ansible/list_hosts.sh
@@ -2,6 +2,6 @@
 
 pushd $(dirname "$0") > /dev/null
 
-ansible-inventory -i inventory.aws_ec2.yml --list | jq '._meta.hostvars[] | {name: .tags.Name, public_ip_address: .public_ip_address, private_ip_address: .private_ip_address}'
+ansible-inventory -i inventory/inventory.aws_ec2.yml --list | jq '._meta.hostvars[] | {name: .tags.Name, public_ip_address: .public_ip_address, private_ip_address: .private_ip_address}'
 
 popd > /dev/null
